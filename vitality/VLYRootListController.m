bfee5114477441b7d003078ee448263f4599e1c7
Shade-Zepheri <ziroalpha@gmail.com>
2018-03-13 21:42:24 -0500

    [prefs] Change appearance in viewDidLoad

--- vitality/VLYRootListController.m
+++ vitality/VLYRootListController.m
@@ -10,19 +10,18 @@
     return @"Root";
 }
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        HBAppearanceSettings *appearanceSettings = [[HBAppearanceSettings alloc] init];
-        appearanceSettings.tintColor = [UIColor colorWithRed:0.98 green:0.66 blue:0.15 alpha:1.0];
-        appearanceSettings.navigationBarTitleColor = [UIColor whiteColor];
-        appearanceSettings.navigationBarBackgroundColor = [UIColor colorWithRed:0.00 green:0.54 blue:0.48 alpha:1.0];
-        appearanceSettings.statusBarTintColor = [UIColor whiteColor];
-        appearanceSettings.translucentNavigationBar = NO;
-        self.hb_appearanceSettings = appearanceSettings;
-    }
+#pragma mark - View management
 
-    return self;
+- (void)viewDidLoad  {
+    [super viewDidLoad];
+
+    HBAppearanceSettings *appearanceSettings = [[HBAppearanceSettings alloc] init];
+    appearanceSettings.tintColor = [UIColor colorWithRed:0.98 green:0.66 blue:0.15 alpha:1.0];
+    appearanceSettings.navigationBarTitleColor = [UIColor whiteColor];
+    appearanceSettings.navigationBarBackgroundColor = [UIColor colorWithRed:0.00 green:0.54 blue:0.48 alpha:1.0];
+    appearanceSettings.statusBarTintColor = [UIColor whiteColor];
+    appearanceSettings.translucentNavigationBar = NO;
+    self.hb_appearanceSettings = appearanceSettings;
 }
 
 #pragma mark - Supporting methods
